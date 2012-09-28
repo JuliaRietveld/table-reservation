a14230f93b7d606b0f83329faaa3131480feb47b
rahul <rahulya@thoughtworks.com>
2012-09-28 15:44:07 +0530

    <Rahul>: Updated functionality.

--- javascript/main.js
+++ javascript/main.js
@@ -1,46 +1,50 @@
 $(document).ready(function(){
     var currentReservation;
+
+  //Code to make things draggable
     $( ".new-reservation").draggable({
         revert: true,
-        start: function(event, ui) {
+        start: function(event,ui){
             currentReservation = $(this);
-            $(this).addClass('dragged');
-            highlightPossibleVacancies();
+            currentReservation.addClass('dragged');
+            highlightSuitableDropSpots();
         },
-        stop: function(event, ui){
-            $(this).removeClass('dragged');
-            unHighlightPossibleVacancies();
+        stop: function(event, ui) {
+            currentReservation.removeClass('dragged');
+            removeHighlightsFromVacantSpots();
         }
     });
-    $('.empty-slot').droppable({
+
+  //Code to make locations behave as drop spots
+    $('.vacant-slot').droppable({
         tolerance:"touch",
         disabled: true,
-         drop: function( event, ui ) {
-//             currentReservation.draggable( "option", {"revert": false,"disabled": true});
-             currentReservation.removeClass('new-reservation dragged')
-                    .addClass('reservation-added')
-                    .draggable( "destroy");
-             currentReservation.appendTo($(this));
-             currentReservation.css({left:0,top:0});
-             unHighlightPossibleVacancies();
-         }
-     });
+        drop: function( event, ui ) {
+            var reservationTime = currentReservation.height();
+            $(this).height($(this).height()-reservationTime);
+            currentReservation.removeClass('new-reservation dragged')
+                .addClass('reservation-added')
+                .draggable('destroy')
+                .insertBefore($(this))
+                .css({left:0,top:0});
+            removeHighlightsFromVacantSpots();
+        }
+    });
 
-
-
-    function highlightPossibleVacancies(){
+    function highlightSuitableDropSpots(){
         var requiredSeats = currentReservation.width(),
             requiredTime = currentReservation.height();
-        $('.empty-slot').each(function(){
+        $('.vacant-slot').each(function(){
             var $this = $(this);
             if($this.height() >= requiredTime &&  $this.width()>=requiredSeats){
-                $this.addClass('matching-vacancy')
+                $this.addClass('highlight-vacancy')
                     .droppable('option','disabled',false);
             }
-        });
+        })
     };
-    function unHighlightPossibleVacancies(){
-       $('.matching-vacancy').removeClass('matching-vacancy')
+
+    function removeHighlightsFromVacantSpots(){
+        $('.highlight-vacancy').removeClass('highlight-vacancy')
             .droppable('option','disabled',true);
-    };
+    }
 });
